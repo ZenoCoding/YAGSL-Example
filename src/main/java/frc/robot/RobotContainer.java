abdc9da0f147ee6952c96569fec33fedb22f7964
fortybyte <66460857+fortybyte@users.noreply.github.com>
2024-03-12 14:06:25 -0700

    urmom

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -111,7 +111,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Auto");
+    return drivebase.getAutonomousCommand("mogged6962");
   }
 
   public void setDriveMode()
